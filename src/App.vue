847e63ea33fd23d0d89f87535e950202826ad594
Roy Ali Hasan <informativepath0987@gmail.com>
2021-10-22 16:18:17 +0500

    "This is Directive Component that contains vue directives "

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,20 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello world" />
     <Nav mes="Hello I am parent" />
+    <Directives />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import Nav from "./components/Nav";
-
+import Directives from "./components/Directives.vue";
 export default {
   name: "App",
   components: {
     HelloWorld,
     Nav,
+    Directives,
   },
 };
 </script>
---
+++ src/components/Directives.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <span>{{ mess }}</span>
+    <br />
+    <button v-on:click="cap">To Capitalize</button>
+    <br>
+    <button v-on:click="small">To Small</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Directives",
+  data() {
+    return {
+      mess: "Ali",
+    };
+  },
+
+  methods: {
+    cap: function () {
+      this.mess = this.mess.toUpperCase();
+    },
+    small: function () {
+      this.mess = this.mess.toLowerCase();
+    },
+  },
+};
+</script>
